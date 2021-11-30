9da7240df7f23073e3aa05ca9fab2fbaab6747d6
Damian Hirsch <damianghirsch@gmail.com>
2021-11-30 08:58:20 +0100

    Setup

---
+++ Day10.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def get_input():
+    with open('Input/Day10.txt', 'r') as file:
+        data = file.read().splitlines()
+    return data
+
+
+def part_one(data: list, rounds: int) -> int:
+
+    return xy
+
+
+def part_two(data: list, rounds: int) -> np.int64:
+
+    return xy
+
+
+def main():
+    print('xy', part_one(get_input(), 100))
+    print('xy', part_two(get_input(), 10000000))
+
+
+if __name__ == '__main__':
+    main()
