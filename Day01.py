b17b14d04cf65ede9bca315fb556b2e3cf200996
Damian Hirsch <damianghirsch@gmail.com>
2021-12-01 11:36:23 +0100

    Day 1

--- Day01.py
+++ Day01.py
@@ -4,6 +4,7 @@ import numpy as np
 # Get data from .txt file
 def get_input():
     with open('Input/Day01.txt', 'r') as file:
+        # Split lines and write each line to list
         data = file.read().splitlines()
         # Map strings in list to integers
         data = list(map(int, data))
